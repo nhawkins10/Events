83e0f791d90af857a685a03134379ae9e135ca0e
nh028808 <nathan.hawkins@cerner.com>
2015-08-25 16:05:52 -0500

    testing date formatting

--- events.js
+++ events.js
@@ -62,8 +62,8 @@ var Events = (function() {
 			dateString += date.getHours() > 12 ? " pm" : " am";
 		} else {
 			dateString = "testing";
-			dateString += months[date.getMonth()];
-			/*dateString += " " + date.getDate();
+			//dateString += months[date.getMonth()];
+			dateString += " " + date.getDate();
 			if (date.getFullYear() < today.getFullYear()) {
 				dateString += ", " + date.getFullYear();
 			}
@@ -72,7 +72,7 @@ var Events = (function() {
 			dateString += "  " + (date.getHours() > 12 ? date.getHours() - 12 : date.getHours());
 			dateString += ":";
 			dateString += date.getMinutes();
-			dateString += date.getHours() > 12 ? " pm" : " am";*/
+			dateString += date.getHours() > 12 ? " pm" : " am";
 		}
 		
 		return dateString;
